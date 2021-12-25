3926720cd45173adffa30dc39309ba52919e8766
Curtis <hong.curtis@gmail.com>
2021-12-24 20:54:10 -0700

    added a prompt and one question

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
-// TODO: Include packages needed for this application
+  // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const test ="test";
+console.log(test);
 
+
+const promptUser = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your README.md?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    return console.log('Please enter a title');
+                }
+        }
+    }, 
+        // what is the title of your project
+
+        // Description 
+        // ToC
+        // Installation
+        // Usage
+        // License
+        // Contributing
+        // Tests 
+        // Questions 
+    ]);
+};   
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +46,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+promptUser();
